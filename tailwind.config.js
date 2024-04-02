0c646c54f0b18c09f07e982cbbf932ba4f474640
Oz996 <ozgun996@hotmail.com>
2024-04-02 19:41:07 +0200

    added font, mixins for texts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "6rem",
+    },
+    extend: {
+      colors: {
+        peach: {
+          DEFAULT: "#E7816B",
+          light: "#FFAD9B",
+        },
+        black: {
+          light: "#1D1C1E",
+        },
+        grey: {
+          DEFAULT: "#333136",
+          light: "#F1F3F5",
+        },
+      },
+    },
   },
   plugins: [],
 };
