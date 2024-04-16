b8074850639d5fea854ac416c63d525b5ab33639
Oz996 <ozgun996@hotmail.com>
2024-04-16 21:11:26 +0200

    added progress bar linked to scroll with framer motion

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,10 +1,16 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import { useScroll, motion } from "framer-motion";
 
 const Layout = () => {
+  const { scrollYProgress } = useScroll();
   return (
     <>
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-1 origin-left bg-peach z-30"
+        style={{ scaleX: scrollYProgress }}
+      />
       <Header />
       <main className="container pt-24 sm:pt-32">
         <Outlet />
