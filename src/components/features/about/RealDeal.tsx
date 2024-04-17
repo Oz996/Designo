506dcc5711e41571bd7335a456ff677cee1bb9e8
Oz996 <ozgun996@hotmail.com>
2024-04-17 18:01:43 +0200

    added animation for about section

--- src/components/features/about/RealDeal.tsx
+++ src/components/features/about/RealDeal.tsx
@@ -1,6 +1,8 @@
+import MotionSection from "../../shared/Motion/MotionSection";
+
 const RealDeal = () => {
   return (
-    <section className="bg-peach-lighter w-full sm:h-[46rem] lg:h-[40rem] rounded-2xl mt-36">
+    <MotionSection className="bg-peach-lighter w-full sm:h-[46rem] lg:h-[40rem] rounded-2xl mt-36">
       <div className="flex flex-col-reverse lg:flex-row h-full max-lg:text-center">
         <div className="bg-two-circle bg-bottom lg:bg-[bottom_left] bg-no-repeat flex flex-col gap-7 w-full justify-center px-5 lg:px-20 max-sm:py-20 h-full overflow-auto">
           <h2 className="heading-md text-peach">The real deal</h2>
@@ -19,7 +21,7 @@ const RealDeal = () => {
         </div>
         <div className="bg-about-real-deal-mobile max-lg:bg-cover sm:bg-about-real-deal-tablet lg:bg-about-real-deal-desktop bg-no-repeat sm:rounded-t-2xl lg:rounded-r-2xl lg:rounded-tl-none lg:w-[60rem] h-[22rem] sm:h-full min-h-1/2" />
       </div>
-    </section>
+    </MotionSection>
   );
 };
 export default RealDeal;
